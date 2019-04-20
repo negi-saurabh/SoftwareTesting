95d1f4a2eb5614d97d620cf2d5ec43f3b313b13d
Khalil Mayyasi <khello1996@gmail.com>
2019-04-20 16:08:54 +0300

    Added display to show the letters in their position in the guessed word if the user guesses them

--- User.py
+++ User.py
@@ -2,7 +2,7 @@ class User:
 	
 	def __init__(self, name):
 		self.name = name
-		self.life = 6 #amount of lifes the user have in the game equivalent to head,body,two arms, two legs in drawing of hangman
+		self.life = 6 #amount of lives the user have in the game equivalent to head,body,two arms, two legs in drawing of hangman
 		self.guesses = [] #List that will contain user's guesses
 		self.guessedWord= False #boolean variable that is true when user won the game and false otherwise
 	
--- main.py
+++ main.py
@@ -13,6 +13,8 @@ def main():
 	print(selectedWord)
 	
 	print("Hello "+ user.name+", the word you need to guess has "+str(len(selectedWord))+" letters\n")#message to tell the user the number of letter of the word he have to guess
+	showGuessPosition(user.guesses,selectedWord)
+	
 	
 	#Loop to keep going until user wins or he runs our of lives
 	while(user.life > 0 and user.guessedWord == False):
@@ -21,9 +23,20 @@ def main():
 		
 		if((len(guess)==1)):
 			if(((ord(guess) >= 65 and ord(guess) <=90) or (ord(guess)>=97 and ord(guess) <=122))):
-				print("correct")
+				if(guess in selectedWord):
+					user.guesses.append(guess)
+					showGuessPosition(user.guesses,selectedWord)
 			else:
 				print("Please enter an English letter")
 		else:
 			print("Please enter a single English letter")
+			
+#method that will display the guessed letter if it is in the word
+def showGuessPosition(guesses,word):
+	for i in word:
+		if i in guesses:
+			print(i,end='')
+		else:
+			print("-",end='')
+	print()
 main()

9f8e7c1655a5bb08826371cb2508e37e8331f274
Saurabh Negi <saurabhnegi@Saurabhs-MBP.fritz.box>
2019-05-05 21:47:23 +0200

    Final changes

--- main.py
+++ main.py
@@ -13,8 +13,13 @@ def main():
 		count = 0
 
 		print("\n")
-
-		user = User(input("Please enter your name to play the game [NOT ONLY JUST NUMBERS or SPACES]\n"))
+		print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Welcome to HANGMAN - The Game of words ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+		print("===============================                Rulebook                ===============================\n"
+			  "::::::   A player name can not have have just numbers, spaces or Alphanumerics ::::::\n"
+			  "::::::   A Player can guess only one alphabet at a time                        ::::::\n"
+			  "::::::   Each wrong guess will result in a penalty                             ::::::\n"
+			  "::::::   Guessing a right alphabet AGAIN wont be penalized                     ::::::\n")
+		user = User(input("Now, Please enter your player name to play the game\n"))
 		for a in user.name:
 			if (a.isalpha()) == True:
 				count += 1
@@ -41,29 +46,29 @@ def main():
 						#if statement to check if the guess is repeated or if it is not part of the word
 						if(guess.casefold() in w.selectedWord.casefold() and guess.casefold() in user.guesses):
 							print("You have already correctly guessed the letter '"+guess+"' and it is part of the word\n") #if letter is correctly repeated lives is not deducted
-							print("\nThe word you need to guess: ")
+							print("\nThe word: ")
 							showGuessPosition(user.guesses,w.selectedWord)
 
 						elif(guess.casefold() not in w.selectedWord.casefold() and guess.casefold() in user.guesses):
 							print("YOU HAVE ALREADY GUESSED THE LETTER '"+guess+"' AND IT IS NOT PART OF THE WORD\n")#if letter is repeated and it is not part of the word, then life is deducted
 							user.life-=1#decrement the lives of the user
 							h.drawHangman(user.life)#call method to add a drawing to the hangman
-							print("\nThe word you need to guess: ")
+							print("\nThe word: ")
 							showGuessPosition(user.guesses,w.selectedWord)
 						else:
 							print("Wrong letter!\n")#if the letter is guessed for the first time and it is not part of the word
 							user.life-=1#decrement the lives of the user
 							h.drawHangman(user.life)#call method to add a drawing to the hangman
-							print("\nThe word you need to guess: ")
+							print("\nThe word: ")
 							showGuessPosition(user.guesses,w.selectedWord)
 						user.guesses.append(guess)
 				else:
 					print("Please enter an English letter\n")
-					print("\nThe word you need to guess: ")
+					print("\nThe word: ")
 					showGuessPosition(user.guesses,w.selectedWord)
 			else:
 				print("Please enter a single English letter\n")
-				print("\nThe word you need to guess: ")
+				print("\nThe word: ")
 				showGuessPosition(user.guesses,w.selectedWord)
 
 		if(user.life == 0):#message if the user loses
